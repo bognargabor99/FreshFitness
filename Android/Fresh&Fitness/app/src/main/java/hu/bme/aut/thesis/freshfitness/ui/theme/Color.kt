885878dddbc843e19e5760587406084a1f61ebe3
Gabor Bela Bognar <gaborbognar99@gmail.com>
2023-03-20 21:50:52 +0100

    Create Android project

---
+++ Android/Fresh&Fitness/app/src/main/java/hu/bme/aut/thesis/freshfitness/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package hu.bme.aut.thesis.freshfitness.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val taupe100 = Color(0xFFF0EAE2)
+val taupe800 = Color(0xFF655454)
+val rust300 = Color(0xFFE1AFAF)
+val rust600 = Color(0xFF886363)
+val gray900 = Color(0xFF333333)
