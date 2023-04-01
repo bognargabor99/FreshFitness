d01f40a0411ca70437ac5c73121fa9852abb2f30
Gabor Bela Bognar <gaborbognar99@gmail.com>
2023-04-01 11:34:02 +0200

    outsource secrets

--- Android/Fresh&Fitness/app/src/main/java/hu/bme/aut/thesis/freshfitness/aws/cognito/CognitoSettings.kt
+++ Android/Fresh&Fitness/app/src/main/java/hu/bme/aut/thesis/freshfitness/aws/cognito/CognitoSettings.kt
@@ -3,11 +3,12 @@ package hu.bme.aut.thesis.freshfitness.aws.cognito
 import android.content.Context
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool
 import com.amazonaws.regions.Regions
+import hu.bme.aut.thesis.freshfitness.BuildConfig
 
 class CognitoSettings(private val context: Context) {
-    private val userPoolId: String = "eu-central-1_GxTKLPzc1"
-    private val clientId: String = "7gvkac478lghc0t96kmuj539bv"
-    private val clientSecret: String = "757ju5k7jvpj2ktoea2i1pftjfn24oku9ruv1vhlls2f1an2sfj"
+    private val userPoolId: String = BuildConfig.COGNITO_USERPOOL_ID
+    private val clientId: String = BuildConfig.COGNITO_CLIENT_ID
+    private val clientSecret: String = BuildConfig.COGNITO_CLIENT_SECRET
     private val cognitoRegion: Regions = Regions.EU_CENTRAL_1
 
     fun getUserPool() : CognitoUserPool {
