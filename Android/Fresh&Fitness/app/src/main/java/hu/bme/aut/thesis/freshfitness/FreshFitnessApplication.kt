46f54795b6a84958b5331e15fbd62a44968969c9
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2023-09-25 23:23:02 +0200

    Add dependencies for API Gateway and S3

--- Android/Fresh&Fitness/app/src/main/java/hu/bme/aut/thesis/freshfitness/FreshFitnessApplication.kt
+++ Android/Fresh&Fitness/app/src/main/java/hu/bme/aut/thesis/freshfitness/FreshFitnessApplication.kt
@@ -4,8 +4,10 @@ import android.app.Application
 import android.util.Log
 import androidx.room.Room
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.api.aws.AWSApiPlugin
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 import hu.bme.aut.thesis.freshfitness.persistence.RunningDatabase
 
 class FreshFitnessApplication : Application() {
@@ -19,6 +21,8 @@ class FreshFitnessApplication : Application() {
         super.onCreate()
 
         try {
+            Amplify.addPlugin(AWSApiPlugin())
+            Amplify.addPlugin(AWSS3StoragePlugin())
             Amplify.addPlugin(AWSCognitoAuthPlugin())
             Amplify.configure(applicationContext)
             Log.i("amplify_init_config", "Amplify was successfully configured")
